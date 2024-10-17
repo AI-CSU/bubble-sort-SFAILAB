7467f5db4db8bee495091f7c129a477ccee03553
SFAILAB <kwy00@naver.com>
2024-10-17 03:11:59 +0000

    main

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,7 +6,7 @@ def bubble_sort(arr):
             # Traverse the array from 0 to n-i-1
             # Swap if the element found is greater than the next element
             if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+                arr[j], arr[j+1] = arr[j+1], arr[j]+1
 
 if __name__ == "__main__":
     # Example usage
